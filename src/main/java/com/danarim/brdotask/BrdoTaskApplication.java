c8ddcd12f2eed290294a6ab185e9eeb5f427ecaf
Nazar Horshevikov <rangar.danarim@gmail.com>
2023-03-04 16:33:54 +0200

    INIT

---
+++ src/main/java/com/danarim/brdotask/BrdoTaskApplication.java
@@ -0,0 +1,13 @@
+package com.danarim.brdotask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrdoTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrdoTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/danarim/brdotask/BrdoTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danarim.brdotask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrdoTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
